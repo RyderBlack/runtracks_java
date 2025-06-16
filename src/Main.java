0dcb4185e0df88fa44d89769272c9644f1961eb4
rydeblack <ryhad.boughanmi@gmail.com>
2025-06-16 09:33:27 +0200

    init repo: runtrack java Day 1 Job 0

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double toto = 3.817;
+        String totoStr = "TOTO";
+        System.out.println("Hello, World and " + totoStr + "!");
+        System.out.println("the number is " + toto);
+
+    }
+}
